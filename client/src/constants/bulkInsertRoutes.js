0e2b4e36a6b8e04a17e8512719b8c99f2b711407
AiwinrajWono <aiwinraj.wono@gmail.com>
2025-07-28 16:53:00 +0530

    started with bulk route listing

--- client/src/constants/bulkInsertRoutes.js
+++ client/src/constants/bulkInsertRoutes.js
@@ -1,11 +1,46 @@
-export default bulkInsertRoutes = [
+const bulkInsertRoutes = [
   {
-    department: "Administration",
+    department: "6798bae6e469e809084e24a4",
     bulkInsertRoutes: [
       {
         name: "assets",
         route: "/api/assets/bulk-insert-assets/6798bae6e469e809084e24a4",
       },
+      {
+        name: "budget",
+        route: "/api/budget/bulk-insert-budget/6798bae6e469e809084e24a4",
+      },
+      {
+        name: "performance",
+        route:
+          "/api/performance/bulk-insert-performance-tasks/6798bae6e469e809084e24a4",
+      },
+      {
+        name: "vendors",
+        route: "/api/vendors/bulk-insert-vendors/6798bae6e469e809084e24a4",
+      },
+      {
+        name: "weekly shift",
+        route: "/api/administration/bulk-insert-weekly-shift-schedule",
+      },
+      {
+        name: "client events",
+        route: "/api/administration/bulk-insert-client-events",
+      },
+      {
+        name: "co-working members",
+        route: "/api/sales/bulk-insert-co-working-client-members",
+      },
+      {
+        name: "house-keeping schedule",
+        route: "/api/company/bulk-insert-housekeeping-schedule",
+      },
+      {
+        name: "inventory",
+        route: "/api/invenotry/bulk-insert-inventory/6798bae6e469e809084e24a4",
+      },
     ],
   },
 ];
+
+export default bulkInsertRoutes;
