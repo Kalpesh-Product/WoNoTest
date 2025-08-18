06c57334643cfd5b3c2d0127911301357bc460c4
muskan-wono <muskan.wono@gmail.com>
2025-08-18 16:16:43 +0530

    added fields

--- server/models/website/WebsiteTemplate.js
+++ server/models/website/WebsiteTemplate.js
@@ -1,35 +1,36 @@
 const mongoose = require("mongoose");
 
 const templateSchema = new mongoose.Schema({
+  searchKey: { type: String, required: true },
   //hero
-  templateName: { type: String, required: true },
+  companyName: { type: String, required: true },
   title: { type: String, required: true },
   subTitle: { type: String, required: true },
   CTAButtonText: { type: String, required: true },
-  heroImages: [
-    {
-      id: { type: String, required: true },
-      url: { type: String, required: true },
-    },
-  ],
-  //about
-  about: { type: String, required: true },
-  //products
-  productTitle: { type: String, required: true },
-  products: [
-    {
-      name: { type: String, required: true },
-      subtitle: { type: String, required: true },
-      cost: { type: String, required: true },
-      description: { type: String, required: true },
-      image: {
-        id: { type: String, required: true },
-        url: { type: String, required: true },
-      },
+  //   heroImages: [
+  //     {
+  //       id: { type: String, required: true },
+  //       url: { type: String, required: true },
+  //     },
+  //   ],
+  //   //about
+  //   about: { type: String, required: true },
+  //   //products
+  //   productTitle: { type: String, required: true },
+  //   products: [
+  //     {
+  //       name: { type: String, required: true },
+  //       subtitle: { type: String, required: true },
+  //       cost: { type: String, required: true },
+  //       description: { type: String, required: true },
+  //       image: {
+  //         id: { type: String, required: true },
+  //         url: { type: String, required: true },
+  //       },
 
-      // productForm???
-    },
-  ],
+  //       // productForm???
+  //     },
+  //   ],
 });
 
 const WebsiteTemplate = mongoose.model("WebsiteTemplate", templateSchema);
