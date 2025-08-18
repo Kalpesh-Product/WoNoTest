df2f83364532f5ded6a9b942c22f11b42251fb6d
muskan-wono <muskan.wono@gmail.com>
2025-08-18 15:48:31 +0530

    added new fields

--- server/models/website/WebsiteTemplate.js
+++ server/models/website/WebsiteTemplate.js
@@ -6,8 +6,30 @@ const templateSchema = new mongoose.Schema({
   title: { type: String, required: true },
   subTitle: { type: String, required: true },
   CTAButtonText: { type: String, required: true },
+  heroImages: [
+    {
+      id: { type: String, required: true },
+      url: { type: String, required: true },
+    },
+  ],
   //about
   about: { type: String, required: true },
+  //products
+  productTitle: { type: String, required: true },
+  products: [
+    {
+      name: { type: String, required: true },
+      subtitle: { type: String, required: true },
+      cost: { type: String, required: true },
+      description: { type: String, required: true },
+      image: {
+        id: { type: String, required: true },
+        url: { type: String, required: true },
+      },
+
+      // productForm???
+    },
+  ],
 });
 
 const WebsiteTemplate = mongoose.model("WebsiteTemplate", templateSchema);
